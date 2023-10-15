ad5d1381af7f58a8d32219159bf7673b007c1ce6
gouranga <gm49302@gmail.com>
2023-10-16 00:29:58 +0530

    basic templete

---
+++ src/test/java/com/example/sundarsaptest001/springbasic/SpringbasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sundarsaptest001.springbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
